74b67a0ceba953057ae2d2c5dc9f94cdbf203b4f
Brian Maleki <hyporos@gmail.com>
2023-09-07 23:26:54 -0400

    /link error handling

--- src/commands/culvert/link.js
+++ src/commands/culvert/link.js
@@ -27,23 +27,36 @@ module.exports = {
     const characterName = interaction.options.getString("character_name");
     const discordUser = interaction.options.getUser("discord_user");
 
-    // Create or update a user entry with a linked character
-    await culvertSchema.findOneAndUpdate(
-      {
-        _id: discordUser.id,
-      },
-      {
-        _id: discordUser.id,
-        $addToSet: { characters: { name: characterName } },
-      },
-      {
-        upsert: true,
-      }
-    );
+    // Check if character is linked to user
+    const characterLinked = await culvertSchema.exists({
+      "characters.name": characterName,
+    });
+
+    // Create or update a user entry and link a character (if not already linked)
+    if (!characterLinked) {
+      await culvertSchema.findOneAndUpdate(
+        {
+          _id: discordUser.id,
+        },
+        {
+          _id: discordUser.id,
+          $addToSet: { characters: { name: characterName } },
+        },
+        {
+          upsert: true,
+        }
+      );
+    }
 
     // Display responses
-    interaction.reply({
-      content: `Linked **${characterName}** to ${discordUser}\nUser ID: ${discordUser.id}`,
-    });
+    let response = "";
+
+    if (characterLinked) {
+      response = `Error ⎯ The character **${characterName}** is already linked`;
+    } else {
+      response = `Linked **${characterName}** to ${discordUser}\nUser ID: ${discordUser.id}`;
+    }
+
+    interaction.reply(response);
   },
 };
