52b20e1d62f668dc44d04bf7554b367977d2d885
Brian Maleki <hyporos@gmail.com>
2023-09-09 03:50:08 -0400

    /roll emoji changes

--- src/commands/fun/roll.js
+++ src/commands/fun/roll.js
@@ -1,15 +1,24 @@
 const { SlashCommandBuilder } = require("discord.js");
 
+// ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ //
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("roll")
     .setDescription("Roll a number between 1 and 100"),
+
+  // ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ //
+
   async execute(client, interaction) {
     const number = Math.floor(Math.random() * 100) + 1;
+    function getEmoji(number) {
+      if (number === 1) return "<:sakuicant:1112235091364683796>";
+      if (number === 69) return "<:sakuSlyL:1091136098098487326>";
+      if (number === 100) return "<:sakuHUH:1134861539728433222>";
+      return "";
+    }
     interaction.reply({
-      content: `You've rolled ${number}! ${
-        number === 69 ? " <:sakuSlyL:1091136098098487326>" : ""
-      }`,
+      content: `You've rolled ${number}! ${getEmoji(number)}`,
     });
   },
 };
